e0a6bba0736432da9bd3c3429a45de376b380402
Leandro Letoile <leandroletoile@hotmail.com>
2022-10-31 11:42:23 -0300

    RentController DevMovie

--- controllers/RentController.js
+++ controllers/RentController.js
@@ -4,6 +4,22 @@ const { Op } = require('sequelize');
 
 
 
+const daysDifference = (start, end) => {
+
+    const dateOne = new Date(start)
+
+    const dateSecond = new Date(end)
+
+    const oneDay = (3600 * 1000 * 24)
+
+    const differenceTime = dateSecond.getTime() - dateOne.getTime()
+
+    const differenceDays = Math.round(differenceTime / oneDay)
+
+    return differenceDays
+}
+
+
 const rentMovie = (req, res, next) => {
 
     const { code } = req.params;
@@ -16,7 +32,6 @@ const rentMovie = (req, res, next) => {
                 id_user: req.user.id,
                 rent_date: Date.now(),
                 refund_date: new Date(Date.now() + (3600 * 1000 * 24) * 7),
-
             }).then(data => {
                 Movie.update({ stock: rental.stock - 1, rentals: rental.rentals + 1 }, { where: { code: rental.code } })
                     .then(() => res.status(201).send(data))
@@ -25,6 +40,29 @@ const rentMovie = (req, res, next) => {
 }
 
 
+const devMovie = (req, res, next) => {
+
+    const { code } = req.params
+
+    Rent.update({ userRefund_date: Date.now() }, { where: { code: code, id_user: req.user.id } })
+        .then(async rent => {
+            let movie = await Movie.findOne({ where: { code: code } })
+            Movie.update({ stock: movie.stock + 1 }, { where: { code: code } })
+                .then(() => {
+                    if (daysDifference(rent.Rent_date, rent.userRefund_date) <= daysDifference(rent.Rent_date, rent.refund_date)) {
+                        
+                        res.status(200).send({ msg: `Entrega a tiempo, Precio final: ${daysDifference(rent.Rent_date, rent.refund_date) * 10}`, onTime: true })
+                    } else {
+                        res.status(200).send({ msg: `Entrega tardia, Precio final: >>>> TODO: FUNCTION PENALTY FEE <<<< `, onTime: false })
+                    }
+                })
+        })
+
+}
+
+
+
 module.exports = {
-    rentMovie
+    rentMovie,
+    devMovie
 }
