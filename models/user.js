36c67488ac5eca2c721ed6dfaec18fdd195e4a6f
Agus_Baez <agustinbaezignacio@gmail.com>
2022-11-03 14:08:13 -0300

    🔨 Update Model User

--- models/user.js
+++ models/user.js
@@ -2,19 +2,12 @@
 const { Model } = require("sequelize");
 module.exports = (sequelize, DataTypes) => {
   class User extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
     static associate(models) {
-      // define association here
-      // User.belongsToMany(models.Movie, { through: "FavouriteFilms" });
     }
   }
   User.init(
     {
-      id: {
+      id_user: {  
         allowNull: false,
         autoIncrement: true,
         primaryKey: true,
