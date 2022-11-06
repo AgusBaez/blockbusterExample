3c59d6f9bd9fd29852eb90c2d5ad82a6fbce0572
Agus_Baez <agustinbaezignacio@gmail.com>
2022-11-06 18:36:54 -0300

    🛠 Update RentController

--- controllers/RentController.js
+++ controllers/RentController.js
@@ -2,17 +2,6 @@ const db = require("../models/index");
 const { Rent, Movie } = db;
 const { Op } = require("sequelize");
 
-//Funcion agregar un 10% del precio original por cada dia de tardanza
-const lateRefund = async (originalPrice, daysLate) => {
-  let finalPrice = originalPrice;
-
-  for (let i = 0; i < daysLate; i++) {
-    finalPrice += finalPrice * 0.1;
-  }
-
-  return finalPrice;
-};
-
 //Calcular los dias de alquiler
 const daysDifference = (start, end) => {
   const dateOne = new Date(start);
@@ -28,27 +17,36 @@ const daysDifference = (start, end) => {
   return differenceDays;
 };
 
-const allRents = (req, res, next) => {
-  let id = req.user.id_user;
+const allRents = async (req, res, next) => {
+  try {
+    let id = req.user.id_user;
 
-  Rent.findOne({
-    where: {
-      userId: id,
-    },
-  }).then((rental) => res.status(200).send(rental));
+    await Rent.findOne({
+      where: {
+        UserId: id,
+      },
+    }).then((rental) => {
+      res.status(200).send(rental);
+    });
+  } catch (error) {
+    console.log(error);
+    error = new Error("The rents does not exist");
+    error.status = 400;
+    res.status(400).send("An error occurred");
+    return next(error);
+  }
 };
 
 const rentMovie = async (req, res, next) => {
-  const paramData = req.params;
+  const { code } = req.params;
 
   await Movie.findOne({
-    where: { code: paramData.code, stock: { [Op.gt]: 0 } },
+    where: { MovieCode: code, stock: { [Op.gt]: 0 } },
   })
-    .then((rental) => {
+    .then(async (rental) => {
       if (!rental) throw new Error(" Missing stock ");
-
-      Rent.create({
-        MovieCode: paramData.code,
+      await Rent.create({
+        MovieMovieCode: code,
         UserId: req.user.id_user,
         rent_date: new Date(Date.now()),
         refund_date: new Date(Date.now() + 3600 * 1000 * 24 * 7),
@@ -56,7 +54,7 @@ const rentMovie = async (req, res, next) => {
         .then((data) => {
           Movie.update(
             { stock: rental.stock - 1, rentals: rental.rentals + 1 },
-            { where: { code: rental.code } }
+            { where: { MovieCode: code } }
           ).then(() => res.status(201).send(data));
         })
         .catch((error) => {
@@ -76,34 +74,50 @@ const rentMovie = async (req, res, next) => {
     });
 };
 
+//Funcion agregar un 10% del precio original por cada dia de tardanza
+const lateRefund = async (originalPrice, daysLate) => {
+  let finalPrice = originalPrice;
+
+  for (let i = 0; i < daysLate; i++) {
+    finalPrice += finalPrice * 0.1;
+  }
+
+  return await finalPrice;
+};
+
 const devMovie = (req, res, next) => {
   const { code } = req.params;
 
   Rent.update(
     { userRefund_date: Date.now() },
-    { where: { code: code, id_user: req.usuario.id_user } }
+    { where: { MovieMovieCode: code, UserId: req.user.id_user } }
   ).then(async (rent) => {
-    let movie = await Movie.findOne({ where: { code: code } });
-    Movie.update({ stock: movie.stock + 1 }, { where: { code: code } }).then(
-      () => {
-        if (
-          daysDifference(rent.Rent_date, rent.userRefund_date) <=
-          daysDifference(rent.Rent_date, rent.refund_date)
-        ) {
-          res.status(200).send({
-            msg: `Entrega a tiempo, Precio final: ${
-              daysDifference(rent.Rent_date, rent.refund_date) * 10
-            }`,
-            onTime: true,
-          });
-        } else {
-          res.status(200).send({
-            msg: `Entrega tardia, Precio final: >>>> TODO: FUNCTION PENALTY FEE <<<< `,
-            onTime: false,
-          });
-        }
+    let movie = await Movie.findOne({ where: { MovieCode: code } });
+    Movie.update(
+      { stock: movie.stock + 1 },
+      { where: { MovieCode: code } }
+    ).then(async () => {
+      if (
+        daysDifference(rent.Rent_date, rent.userRefund_date) <=
+        daysDifference(rent.Rent_date, rent.refund_date)
+      ) {
+        res.status(200).send({
+          msg: `Entrega a tiempo, Precio final: ${
+            daysDifference(rent.Rent_date, rent.refund_date) * 10
+          }`,
+          onTime: true,
+        });
+      } else {
+        console.log();
+        res.status(200).send({
+          msg: `Entrega tardia, Precio final: ${await lateRefund(
+            400,
+            daysDifference(rent.Rent_date, rent.userRefund_date)
+          )} `,
+          onTime: false,
+        });
       }
-    );
+    });
   });
 };
 
