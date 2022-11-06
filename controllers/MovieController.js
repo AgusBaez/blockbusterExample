27b22b313ced48db2e8891b2d794863c0d8cb2a0
Agus_Baez <agustinbaezignacio@gmail.com>
2022-11-06 18:36:32 -0300

    🛠 Update MovieController

--- controllers/MovieController.js
+++ controllers/MovieController.js
@@ -5,28 +5,28 @@ const db = require("../models/index");
 const { Movie, FavouriteFilms } = db;
 
 const getMovies = async (req, res) => {
-try {
-  let movies = await fetch(GHIBLI_APP);
-  movies = await movies.json();
+  try {
+    let movies = await fetch(GHIBLI_APP);
+    movies = await movies.json();
 
-  movies = movies.map((movie) => ({
-    id: movie.id,
-    title: movie.title,
-    description: movie.description,
-    director: movie.director,
-    producer: movie.producer,
-    release_date: movie.producer,
-    running_time: movie.running_time,
-    rt_score: movie.rt_score,
-  }));
-  
-  res.status(200).send(movies);
-} catch (error) {
-  error = new Error("Oww.. Movie Not Found");
-  error.status = 400;
-  res.status(400).send("Movie Not Found");
-  return next(error);
-}
+    movies = movies.map((movie) => ({
+      id: movie.id,
+      title: movie.title,
+      description: movie.description,
+      director: movie.director,
+      producer: movie.producer,
+      release_date: movie.producer,
+      running_time: movie.running_time,
+      rt_score: movie.rt_score,
+    }));
+
+    res.status(200).send(movies);
+  } catch (error) {
+    error = new Error("Oww.. Movie Not Found");
+    error.status = 400;
+    res.status(400).send("Movie Not Found");
+    return next(error);
+  }
 };
 
 //getmovie By Title por Body
@@ -78,34 +78,52 @@ const getMoviesByRuntime = async (req, res) => {
 };
 
 const getMovieDetails = async (req, res) => {
-  const { id } = req.params;
-  let movies = await fetch("https://ghibliapi.herokuapp.com/films");
-  movies = await movies.json();
-  movies = movies.map((movie) => ({
-    id: movie.id,
-    title: movie.title,
-    description: movie.description,
-    director: movie.director,
-    producer: movie.producer,
-    release_date: movie.producer,
-    running_time: movie.running_time,
-    rt_score: movie.rt_score,
-  }));
-  const movie = movies.find((movie) => movie.id === id);
-  res.status(200).send(movie);
+  try {
+    const { id } = req.params;
+    let movies = await fetch("https://ghibliapi.herokuapp.com/films");
+    movies = await movies.json();
+
+    movies = movies.map((movie) => ({
+      id: movie.id,
+      title: movie.title,
+      description: movie.description,
+      director: movie.director,
+      producer: movie.producer,
+      release_date: movie.producer,
+      running_time: movie.running_time,
+      rt_score: movie.rt_score,
+    }));
+
+    const movie = movies.find((movie) => movie.id === id);
+
+    res.status(200).send(movie);
+  } catch (error) {
+    error = new Error("Movie Not Found");
+    error.status = 400;
+    res.status(400).send("Oww.. Movie Not Found");
+    return next(error);
+  }
 };
 
 const addMovie = (req, res, next) => {
-  const movie = getFilmFromAPIByName(req.body.title);
-  const newMovie = {
-    code: movie.id,
-    title: movie.title,
-    stock: 5,
-    rentals: 0,
-  };
-  Movie.create(newMovie)
-    .then((movie) => res.status(201).send("Movie Stocked"))
-    .catch((err) => next(err));
+  try {
+    const movie = getFilmFromAPIByName(req.body.title);
+    const newMovie = {
+      code: movie.id,
+      title: movie.title,
+      stock: 5,
+      rentals: 0,
+    };
+    Movie.create(newMovie).then((movie) =>
+      res.status(201).send("Movie Stocked")
+    );
+  } catch (error) {
+    console.log(error);
+    error = new Error("Warning: Error addMovie");
+    error.status = 400;
+    res.status(400).send("Warning ADMIN: Error addMovie");
+    return next(error);
+  }
 };
 
 const addFavourite = async (req, res, next) => {
@@ -113,43 +131,67 @@ const addFavourite = async (req, res, next) => {
     const code = req.params.code;
     const { review } = req.body;
 
-    Movie.findOne({ where: { code: code } }).then((film) => {
+    Movie.findOne({ where: { MovieCode: code } }).then((film) => {
       if (!film) throw new Error(" Pelicula no disponible ");
 
-      const newFavouriteFilms = {
-        MovieCode: film.code,
-        UserId: req.user.id,
+      FavouriteFilms.create({
+        MovieCode: code,
+        id_user: req.user.id_user,
         review: review,
-      };
-
-      FavouriteFilms.create(newFavouriteFilms).then((newFav) => {
-        if (!newFav) throw new Error("FAILED to add favorite movie");
-
-        res.status(201).send("Movie Added to Favorites");
-      });
+      })
+        .then((newFav) => {
+          res.status(201).send("Movie Added to Favorites");
+        })
+        .catch((error) => {
+          console.log(error);
+          error = new Error("FAILED to add favorite movie");
+          error.status = 400;
+          res
+            .status(400)
+            .send(
+              "BlockBuster says that adding this movie to favorites is impossible, because it is already added."
+            );
+          return next(error);
+        });
     });
   } catch (error) {
-    (error) => next(error);
+    console.log(error);
+    error = new Error("Warning: Error addFavorite");
+    error.status = 400;
+    res.status(400).send("The film does not exist");
+    return next(error);
   }
 };
 
 const allFavouritesMovies = async (req, res, next) => {
-  const allFilms = await FavouriteFilms.findAll({
-    where: { UserId: req.user.id },
-  });
+  try {
+    const allFilms = await FavouriteFilms.findAll({
+      where: { id_user: req.user.id },
+    });
 
-  const filmReduced = allFilms.map((film) => {
-    if (film.review != null) {
-      return film;
-    } else {
-      return {
-        id: film.id,
-        MovieCode: film.MovieCode,
-        UserId: film.UserId,
-      };
-    }
-  });
-  res.status(200).json(filmReduced);
+    const filmReduced = allFilms.map((film) => {
+      if (film.review != null) {
+        return film;
+      } else {
+        return {
+          id: film.id,
+          MovieCode: film.MovieCode,
+          id_user: film.UserId,
+        };
+      }
+    });
+    res.status(200).json(filmReduced);
+  } catch (error) {
+    console.log(error);
+    error = new Error("Warning: Error Show Favorites");
+    error.status = 400;
+    res
+      .status(400)
+      .send(
+        "BlockBuster says sorry, a problem occurs when Showing Favorite Movies."
+      );
+    return next(error);
+  }
 };
 
 module.exports = {
